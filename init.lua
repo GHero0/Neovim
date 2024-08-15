040d337f7324b208d55270e41ebaafb1c7c8d745
G0Hero <george13vladut@gmail.com>
2024-08-15 06:22:48 +0200

    Adding LSP toggle-term

--- init.lua
+++ init.lua
@@ -13,3 +13,4 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-opt")
 require("lazy").setup("plugins")
+
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "folke/trouble.nvim",
+        dependencies = {"nvim-tree/nvim-web-devicons"},
+        config = function()
+            require("trouble").setup({})
+            vim.keymap.set("n", "<C-t>", ":Trouble diagnostics <CR>")
+        end
+    }
+}
+
